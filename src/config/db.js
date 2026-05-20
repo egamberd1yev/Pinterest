29ad134bfb3aafd9eade6a25b6f76e82644db4d3
egamberd1yev <jorabekegamberdiyev12@gmail.com>
2026-05-20 23:04:43 +0500

    config written

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,3 @@
-// // src/db.js
-// import { DataSource } from "typeorm";
-// import { UserEntity } from "../models/user.entity.js";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// export const AppDataSource = new DataSource({
-//   type: "postgres",
-//   host: process.env.DB_HOST || "localhost",
-//   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-//   username: process.env.DB_USER || "postgres",
-//   password: process.env.DB_PASSWORD || "secret",
-//   database: process.env.DB_NAME || "pinterest_db",
-//   synchronize: true,
-//   logging: true,
-//   entities: [UserEntity],
 // // import pkg from "pg"
 // // import dotenv from "dotenv"
 
