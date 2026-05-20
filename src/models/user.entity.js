1a3d331e1101d6a68612fdf5ec8d00de06b02756
OtabekMaxDev <o123mahkamov@gmail.com>
2026-05-20 21:46:49 +0500

    user.entity.js qo'shildi va yana packagelar yuklandi .env va gitignore ham qo'shildi

---
+++ src/models/user.entity.js
@@ -0,0 +1,41 @@
+import { EntitySchema } from "typeorm";
+
+export const UserEntity = new EntitySchema({
+  name: "User", //klasssss name
+  tableName: "users", //postgress table name
+  columns: {
+    id: {
+      primary: true,
+      type: "int",
+      generated: true,
+    },
+    username: {
+      type: "varchar",
+      length: 50,
+      unique: true,
+      nullable: false,
+    },
+    email: {
+      type: "varchar",
+      length: 100,
+      unique: true,
+      nullable: false,
+    },
+    password: {
+      type: "varchar",
+      length: 255,
+      nullable: false,
+    },
+    role: {
+      type: "enum",
+      enum: ["user", "admin"],
+      default: "user",
+      nullable: false
+    },
+    createdAt: {
+      name: "created_at",
+      type: "timestamp",
+      createdDate: true, //Auto current time addition
+    }
+  }
+})
